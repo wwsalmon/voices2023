766240f1338a37578f02d517716723fdff143a42
Samson Zhang <wwsamson@yahoo.com>
2023-07-09 15:21:26 -0700

    header links

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ wp_head();
 ?>
 </head>
 <body>
-    <div class="sticky top-0 left-0 right-0">
+    <div class="sticky top-0 left-0 right-0 z-10">
         <div class="h-2 md:h-[10px] bg-tpurple w-full"></div>
         <div class="h-16 md:h-20 w-full flex items-center bg-white relative">
             <button class="md:hidden block ml-4 text-lg opacity-50 hover:opacity-100"><i class="fa-solid fa-bars"></i></button>
@@ -19,9 +19,9 @@ wp_head();
                 <a href="https://aaja.org/" class="block h-full px-4 mr-4 flex items-center justify-center opacity-80 hover:opacity-100 bg-gray-100">
                     <img src="<?php echo get_template_directory_uri() . '/img/aaja.jpg'; ?>" alt="AAJA logo" class="grayscale h-9 mix-blend-multiply">
                 </a>
-                <a href="" class="font-semibold mx-4">About</a>
-                <a href="" class="font-semibold mx-4">People</a>
-                <a href="" class="font-semibold mx-4">Donate</a>
+                <a href="<?php echo home_url("/about")?>" class="font-semibold mx-4">About</a>
+                <a href="<?php echo home_url("/people")?>" class="font-semibold mx-4">People</a>
+                <a href="https://aaja-official.salsalabs.org/donate-programs/index.html" class="font-semibold mx-4">Donate</a>
             </div>
             <a href="<?php echo home_url() ?>" class="absolute left-1/2 top-4 md:top-5 transform -translate-x-1/2">
                 <img src="<?php echo get_template_directory_uri() . '/img/voices.png'; ?>" class="h-8 md:h-9" alt="">
@@ -33,7 +33,7 @@ wp_head();
                 <div class="dropdown">
                     <a href="" class="font-black mx-4">Student work <i class="fa-solid fa-caret-down"></i></a>
                 </div>
-                <a href="" class="block px-3 h-10 flex items-center justify-center uppercase bg-tred font-semibold text-white">
+                <a href="<?php echo home_url("/apply")?>" class="block px-3 h-10 flex items-center justify-center uppercase bg-tred font-semibold text-white">
                     Apply now
                 </a>
             </div>
