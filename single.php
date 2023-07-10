dd3e03a3bf09c6dc495a53461f93ef0a2d04eb67
Samson Zhang <wwsamson@yahoo.com>
2023-07-09 18:27:28 -0700

    fix article page tag

--- single.php
+++ single.php
@@ -3,8 +3,11 @@
 <?php
     while (have_posts()) : the_post();?>
     <div class="max-w-5xl mx-auto px-4 py-12">
-        <p class="text-tred uppercase font-black tracking-[1.6px]"><?php echo get_the_tags()[0]->name ?></p>
-        <p class="font-ss"><a href="<?php echo home_url("/stories")?>" class="underline">Student work</a> • <a href="" class="italic underline"><?php echo get_the_category()[0]->name ?></a></p>
+        <?php
+        $tags = array_values(array_filter(get_the_tags(), function($t) {return $t->name != "Featured";}));
+        ?>
+        <p class="text-tred uppercase font-black tracking-[1.6px]"><?php echo $tags[0]->name ?></p>
+        <p class="font-ss"><a href="<?php echo home_url("/stories")?>" class="underline">Student work</a> • <a href="<?php echo get_category_link(get_the_category()[0]) ?>" class="italic underline"><?php echo get_the_category()[0]->name ?></a></p>
         <h1 class="font-ss font-black text-5xl sm:text-6xl md:text-7xl max-w-4xl mt-4 mb-8 sm:mb-16" style="line-height: 1.1;"><?php the_title() ?></h1>
         <div class="md:flex">
             <div class="md:ml-12 md:mt-0 order-2">
