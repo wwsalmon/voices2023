0bcf425a41ea1c44b908a55e5d6fe5dbad1bd0fd
Samson Zhang <wwsamson@yahoo.com>
2023-07-11 01:51:18 -0700

    close #6

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ wp_head();
     <div class="sticky top-0 left-0 right-0 z-10">
         <div class="h-2 md:h-[10px] bg-tpurple w-full"></div>
         <div class="h-16 md:h-20 w-full flex items-center bg-white relative">
-            <button class="md:hidden block ml-4 text-lg opacity-50 hover:opacity-100"><i class="fa-solid fa-bars"></i></button>
+            <button class="md:hidden block ml-4 text-lg opacity-50 hover:opacity-100" id="bars"><i class="fa-solid fa-bars"></i></button>
             <div class="md:flex items-center h-full hidden">
                 <a href="https://aaja.org/" class="block h-full px-4 mr-4 flex items-center justify-center opacity-80 hover:opacity-100 bg-gray-100">
                     <img src="<?php echo get_template_directory_uri() . '/img/aaja.jpg'; ?>" alt="AAJA logo" class="grayscale h-9 mix-blend-multiply">
@@ -39,3 +39,30 @@ wp_head();
             </div>
         </div>
     </div>
+    <div class="fixed top-0 left-0 bottom-0 w-64 bg-white z-20 shadow-lg p-4 md:hidden" id="mobile-menu">
+        <button class="mb-8 mt-3" id="exit"><i class="fa-solid fa-xmark"></i></button>
+        <a href="https://aaja.org/" class="h-16 mb-8 relative block">
+            <img src="<?php echo get_template_directory_uri() . '/img/aaja.jpg'; ?>" alt="AAJA logo" class="grayscale h-full mix-blend-multiply">
+        </a>
+        <a href="<?php echo home_url("/about")?>" class="font-semibold mb-8 block">About</a>
+        <a href="<?php echo home_url("/people")?>" class="font-semibold mb-8 block">People</a>
+        <a href="https://aaja-official.salsalabs.org/donate-programs/index.html" class="font-semibold mb-8 block">Donate</a>
+        <a href="" class="font-black mb-8 block">Student work <i class="fa-solid fa-caret-down"></i></a>
+        <a href="<?php echo home_url("/apply")?>" class="block px-3 h-10 inline-flex items-center justify-center uppercase bg-tred font-semibold text-white">
+            Apply now
+        </a>
+    </div>
+    <script>
+const menu = document.getElementById("mobile-menu");
+const bars = document.getElementById("bars");
+const exit = document.getElementById("exit");
+
+console.log(menu, bars, exit);
+
+function toggleMenu() {
+    menu.classList.toggle("hidden");
+}
+
+bars.onclick = toggleMenu;
+exit.onclick = toggleMenu;
+    </script>
