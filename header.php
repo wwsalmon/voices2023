c675acb5b240b0f460a343cad43a847f11735a21
Samson Zhang <wwsamson@yahoo.com>
2023-07-11 19:59:14 -0700

    add mobile student work by cohort dropdown

--- header.php
+++ header.php
@@ -31,12 +31,12 @@ wp_head();
             </div>
             <div class="hidden sm:flex items-center ml-auto mr-4">
                 <div class="hidden lg:block relative" id="dropdown">
-                    <a href="<?php echo home_url("/stories") ?>" class="font-black mx-4">Student work <i class="fa-solid fa-caret-down"></i></a>
+                    <a href="<?php echo home_url("/stories") ?>" class="font-black mx-4">Student work <i class="fa-solid fa-caret-down ml-2"></i></a>
                     <div id="dropdown-child" class="hidden absolute bg-white top-6 right-4 rounded shadow-lg whitespace-nowrap">
                         <?php
                         $categories = get_sorted_categories();
                         foreach($categories as $category): ?>
-                            <a href="<?php echo get_category_link($category->term_id) ?>" class="font-semibold px-4 py-2 block hover:bg-gray-100"><?php echo $category->name ?></a>
+                            <a href="<?php echo get_category_link($category->term_id) ?>" class="px-4 py-2 block hover:bg-gray-100"><?php echo $category->name ?></a>
                         <?php endforeach; ?>
                     </div>
                 </div>
@@ -51,25 +51,31 @@ wp_head();
             </div>
         </div>
     </div>
-    <div class="fixed top-0 left-0 bottom-0 w-64 bg-white z-20 shadow-lg p-4 lg:hidden" id="mobile-menu">
-        <button class="mb-8 mt-3" id="exit"><i class="fa-solid fa-xmark"></i></button>
-        <a href="https://aaja.org/" class="h-16 mb-8 relative block">
-            <img src="<?php echo get_template_directory_uri() . '/img/aaja.jpg'; ?>" alt="AAJA logo" class="grayscale h-full mix-blend-multiply">
-        </a>
-        <a href="<?php echo home_url("/about")?>" class="font-semibold mb-8 block">About</a>
-        <a href="<?php echo home_url("/people")?>" class="font-semibold mb-8 block">People</a>
-        <a href="https://aaja-official.salsalabs.org/donate-programs/index.html" class="font-semibold mb-8 block">Donate</a>
-        <a href="" class="font-black mb-8 block">Student work <i class="fa-solid fa-caret-down ml-2"></i></a>
-        <a href="<?php echo home_url("/apply")?>" class="block px-3 h-10 inline-flex items-center justify-center uppercase bg-tred font-semibold text-white">
-            Apply now
-        </a>
+    <div class="fixed top-0 left-0 bottom-0 w-64 bg-white z-20 shadow-lg overflow-y-auto lg:hidden hidden" id="mobile-menu">
+        <div class="p-4">
+            <button class="mb-8 mt-3" id="exit"><i class="fa-solid fa-xmark"></i></button>
+            <a href="https://aaja.org/" class="h-16 mb-8 relative block">
+                <img src="<?php echo get_template_directory_uri() . '/img/aaja.jpg'; ?>" alt="AAJA logo" class="grayscale h-full mix-blend-multiply">
+            </a>
+            <a href="<?php echo home_url("/about")?>" class="font-semibold mb-8 block">About</a>
+            <a href="<?php echo home_url("/people")?>" class="font-semibold mb-8 block">People</a>
+            <a href="https://aaja-official.salsalabs.org/donate-programs/index.html" class="font-semibold mb-8 block">Donate</a>
+            <a href="" class="font-black mb-8 block">Student work</a>
+            <button id="cohort-button" class="block mb-8 font-semibold">By cohort  <i class="fa-solid fa-caret-down ml-2 text-tlightgray"></i></button>
+            <div id="cohort-menu" class="hidden">
+                <?php foreach($categories as $category): ?>
+                    <a href="<?php echo get_category_link($category->term_id) ?>" class="font-semibold pl-4 mb-8 block hover:bg-gray-100"><?php echo $category->name ?></a>
+                <?php endforeach; ?>
+            </div>
+            <a href="<?php echo home_url("/apply")?>" class="block px-3 h-10 inline-flex items-center justify-center uppercase bg-tred text-white">
+                Apply now
+            </a>
+        </div>
     </div>
     <script>
 const menu = document.getElementById("mobile-menu");
 const bars = document.getElementById("bars");
 const exit = document.getElementById("exit");
-
-console.log(menu, bars, exit);
 
 function toggleMenu() {
     menu.classList.toggle("hidden");
@@ -77,4 +83,13 @@ function toggleMenu() {
 
 bars.onclick = toggleMenu;
 exit.onclick = toggleMenu;
+
+const cohort = document.getElementById("cohort-button");
+const cohortMenu = document.getElementById("cohort-menu")
+
+function toggleCohorts() {
+    cohortMenu.classList.toggle("hidden");
+}
+
+cohort.onclick = toggleCohorts;
     </script>
