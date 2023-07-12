8cf7a32fa8843502e0a2a42c3ba16d5c895b9444
Samson Zhang <wwsamson@yahoo.com>
2023-07-11 23:46:00 -0700

    add 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header();
+?>
+    <div class="max-w-xl mx-auto py-24 px-4">
+        <h1 class="text-4xl sm:text-5xl md:text-6xl font-black italic text-tpurple leading-none uppercase mb-8">Page not found</h1>
+        <div class="content">
+            <p>No page was found at this URL. Please make sure you've got it right and try again.</p>
+        </div>
+    </div>
+<?php
+get_footer();
